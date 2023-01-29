fa0b8d44151da67fb51a4e5ec0c1e7684f781f6c
aspen-yryr <aspen_development@outlook.com>
2023-01-29 19:35:33 +0900

    Fix: Remove update team logic

--- client/tmb-client/src/app/module/match/match.component.ts
+++ client/tmb-client/src/app/module/match/match.component.ts
@@ -1,11 +1,7 @@
-import {
-  CdkDragDrop,
-  moveItemInArray,
-} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import { Team } from '../../model/team'
 import { MatchService } from 'src/app/service/match.service';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-match',
@@ -13,42 +9,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./match.component.css']
 })
 export class MatchComponent implements OnInit {
-  teamA = new Team(0);
-  teamB = new Team(1);
+  teamA: User[] = [];
+  teamB: User[] = [];
 
   constructor(
     private readonly svc: MatchService,
     private readonly route: ActivatedRoute
   ) { }
 
-  // TODO: Use ngrx
-  async drop(event: CdkDragDrop<Team>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data.players,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      if (event.previousContainer.data.id === 0) {
-        this.svc.remove('teamA', event.previousContainer.data.players[event.previousIndex])
-        this.svc.append('teamB', event.previousContainer.data.players[event.previousIndex])
-      } else if (event.previousContainer.data.id === 1) {
-        this.svc.remove('teamB', event.previousContainer.data.players[event.previousIndex])
-        this.svc.append('teamA', event.previousContainer.data.players[event.previousIndex])
-      }
-      const id = Number(this.route.snapshot.paramMap.get('id'));
-      const teams = await this.svc.get(id)
-      this.teamA.players = teams[0]
-      this.teamB.players = teams[1]
-    }
-  }
-
   async ngOnInit(): Promise<void> {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     const teams = await this.svc.get(id)
-    this.teamA.players = teams[0]
-    this.teamB.players = teams[1]
+    this.teamA = teams[0]
+    this.teamB = teams[1]
     return
   }
 }
--- client/tmb-client/src/app/service/match.service.ts
+++ client/tmb-client/src/app/service/match.service.ts
@@ -17,36 +17,6 @@ export class MatchService {
     req.setMatchId(id);
     return (await this.client.find(req, null)).toObject();
   }
-  // TODO: Make user service
-  teamA: User[] = [
-    new User(1, 'Player1'),
-    new User(2, 'Player2'),
-  ];
-  teamB: User[] = [
-    new User(3, 'Player3'),
-    new User(4, 'Player4'),
-    new User(5, 'Player5'),
-  ];
-
-  append(team: string, user: User) {
-    if (team === "teamA") {
-      this.teamA.push(user)
-    } else if (team == "teamB") {
-      this.teamB.push(user)
-    }
-    console.log(this.teamA)
-    console.log(this.teamB)
-  }
-
-  remove(team: string, user: User) {
-    if (team === "teamA") {
-      this.teamA = this.teamA.filter((u) => u.id != user.id)
-    } else if (team == "teamB") {
-      this.teamB = this.teamB.filter((u) => u.id != user.id)
-    }
-    console.log(this.teamA)
-    console.log(this.teamB)
-  }
 
   async get(id: number): Promise<User[][]> {
     const match = await this.find(id);
